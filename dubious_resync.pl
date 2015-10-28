9a71d34c96cc561bf2131f90dcc3ca741d58c081
Dave Lambley <git@davel.me.uk>
2015-10-28 14:14:07 +0000

    Hack to work.

--- dubious_resync.pl
+++ dubious_resync.pl
@@ -8,31 +8,31 @@ open(my $raid_fh, "<", "/dev/md4") or die $!;
 open(my $bad_fh, "<", "/dev/sdd1") or die $!;
 open(my $old_fh, "<", "/dev/sdf1") or die $!;
 
-my $last_read;
+my $last_read=-1;
 my $last_read_buf;
 my $ptr = 0;
 my $block_size = 512;
 my $bad_chunk;
 
-while ($ptr<1943748544) {
+while ($ptr<(1943748544*1024)) {
 	sysseek($bad_fh, $ptr, 0) or die $!;
     if (sysread($bad_fh, my $bad_buf, $block_size)==$block_size) {
         if ($bad_chunk) {
-            sysseek(my $raid_fh, $last_good, 0) or die $!;
-            sysread(my $raid_fh, my $raid_last_good, $block_size)==$block_size or die "could not read from raid: $!";
-            sysseek(my $old_fh, $last_good, 0) or die $!;
-            sysread(my $raid_fh, my $old_last_good, $block_size)==$block_size or die "could not read from old: $!";
+            sysseek($raid_fh, $last_read, 0) or die $!;
+            sysread($raid_fh, my $raid_last_good, $block_size)==$block_size or die "could not read from raid: $!";
+            sysseek($old_fh, $last_read, 0) or die $!;
+            sysread($old_fh, my $old_last_good, $block_size)==$block_size or die "could not read from old: $!";
 
-            sysseek(my $raid_fh, $ptr, 0) or die $!;
-            sysread(my $raid_fh, my $raid_next_good, $block_size)==$block_size or die "could not read from raid: $!";
-            sysseek(my $old_fh, $ptr, 0) or die $!;
-            sysread(my $raid_fh, my $old_next_good, $block_size)==$block_size or die "could not read from old: $!";
+            sysseek($raid_fh, $ptr, 0) or die $!;
+            sysread($raid_fh, my $raid_next_good, $block_size)==$block_size or die "could not read from raid: $!";
+            sysseek($old_fh, $ptr, 0) or die $!;
+            sysread($old_fh, my $old_next_good, $block_size)==$block_size or die "could not read from old: $!";
 
             if ($last_read_buf eq $raid_last_good && $last_read_buf eq $old_last_good && $bad_buf eq $raid_next_good && $bad_buf eq $old_next_good) {
-                print "Good to recover $last_good to $ptr\n";
+                print "Good to recover $last_read to $ptr\n";
             }
             else {
-                print "Dubious to recover $last_good to $ptr\n";
+                print "Dubious to recover $last_read to $ptr\n";
             }
         }
 

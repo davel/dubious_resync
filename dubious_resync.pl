a1e6738b4c7c8b8bb529b4000aea72c776f858ac
Dave Lambley <git@davel.me.uk>
2015-10-29 12:32:06 +0000

    Catch more cases.

--- dubious_resync.pl
+++ dubious_resync.pl
@@ -11,25 +11,32 @@ open(my $old_fh, "<", "/dev/sdf1") or die $!;
 my $last_read=-1;
 my $last_read_buf;
 my $ptr = 0;
-my $block_size = 512;
+my $block_size = 4096;
 my $bad_chunk;
 
 while ($ptr<(1943748544*1024)) {
-	sysseek($bad_fh, $ptr, 0) or die $!;
+    sysseek($bad_fh, $ptr, 0) or die $!;
     if (sysread($bad_fh, my $bad_buf, $block_size)==$block_size) {
         if ($bad_chunk) {
+            # Read unreadable chunk along with the blocks either side.
+            my $chunk_from_old = $ptr-$last_read+$block_size;
+            print "$chunk_from_old $ptr $block_size\n";
+
             sysseek($raid_fh, $last_read, 0) or die $!;
-            sysread($raid_fh, my $raid_last_good, $block_size)==$block_size or die "could not read from raid: $!";
-            sysseek($old_fh, $last_read, 0) or die $!;
-            sysread($old_fh, my $old_last_good, $block_size)==$block_size or die "could not read from old: $!";
+            sysread($raid_fh, my $raid_chunk, $chunk_from_old)==$chunk_from_old or die "could not read from raid: $!";
+            sysseek($old_fh,  $last_read, 0) or die $!;
+            sysread($old_fh, my $old_chunk,   $chunk_from_old)==$chunk_from_old or die "could not read from old: $!";
 
-            sysseek($raid_fh, $ptr, 0) or die $!;
-            sysread($raid_fh, my $raid_next_good, $block_size)==$block_size or die "could not read from raid: $!";
-            sysseek($old_fh, $ptr, 0) or die $!;
-            sysread($old_fh, my $old_next_good, $block_size)==$block_size or die "could not read from old: $!";
-
-            if ($last_read_buf eq $raid_last_good && $last_read_buf eq $old_last_good && $bad_buf eq $raid_next_good && $bad_buf eq $old_next_good) {
-                print "Good to recover $last_read to $ptr\n";
+            if ($raid_chunk eq $old_chunk) {
+                print "Chunk from $last_read to $ptr on raid matches old, no need to fix?\n";
+	    }
+            elsif ($last_read_buf eq substr($raid_chunk, 0, $block_size) && $last_read_buf eq substr($old_chunk, 0, $block_size) && $bad_buf eq substr($raid_chunk, -$block_size, $block_size) && $bad_buf eq substr($old_chunk, -$block_size, $block_size)) {
+                if (substr($raid_chunk, $block_size, $chunk_from_old-$block_size*2) eq ("\x00" x ($chunk_from_old-$block_size*2))) {
+                    print "Good to recover $last_read to $ptr\n";
+                }
+                else {
+                    print "Appears to have been written to: $last_read to $ptr\n";
+                }
             }
             else {
                 print "Dubious to recover $last_read to $ptr\n";

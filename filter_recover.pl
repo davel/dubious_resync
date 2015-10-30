78fb4761151a66a31e5389289d7516fcef0561ba
Dave Lambley <github@davel.me.uk>
2015-10-30 19:36:38 +0000

    Scan for dubious.

--- filter_recover.pl
+++ filter_recover.pl
@@ -12,9 +12,19 @@ my $block_size = 4096;
 while (my $line = <>) {
     if ($line =~ /(\d+) to (\d+)/) {
         my ($last_good, $next_good) = ($1, $2);
-        print $line;
         my $bad_blocks = $next_good-$last_good-$block_size;
         print "last_good=$last_good, next_good=$next_good, blocks=$bad_blocks\n";
+        sysseek($raid_fh, $last_good+4096, 0) or die $!;
+        sysseek($old_fh, $last_good+4096, 0) or die $!;
+
+        sysread($raid_fh, my $raid_dump, $bad_blocks)==$bad_blocks or die "flailed read: $!";
+        sysread($old_fh,  my $old_dump, $bad_blocks)==$bad_blocks or die "flailed read: $!";
+        if ($raid_dump eq $old_dump) {
+            print "No need for action: $line";
+        }
+        else {
+            print "Still dubious: $line";
+        }
     }
 }
 
